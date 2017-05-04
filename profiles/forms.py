3ff02c709145f4421f762f03851fe3d81dc072e6
m431m <m431m@tutanota.com>
2017-05-04 17:13:28 +0200

    Dev

--- profiles/forms.py
+++ profiles/forms.py
@@ -4,64 +4,88 @@ from django.contrib.auth.models import User
 from django.core import validators
 from django.core.mail import send_mail
 from django.forms.utils import ErrorList
-from django.template import Context, Template
 
 from profiles.models import Profile, Organisation
 
 import datetime
 
 
-USERNAME = forms.CharField(
-    label="Nom d'utilisateur",
-    widget=forms.TextInput(
-        attrs={'placeholder': "Nom d'utilisateur", 'class': 'form-control'}),
-    max_length=30,
-    min_length=3,
-    validators=[validators.validate_slug])
-
-
-PASSWORD1 = forms.CharField(
-    label='Mot de passe',
-    max_length=50,
-    min_length=6,
-    widget=forms.PasswordInput(
-        attrs={'placeholder': 'Mot de passe', 'class': 'form-control'}))
-
-
-PASSWORD2 = forms.CharField(
-    label='Confirmer le mot de passe',
-    max_length=50,
-    min_length=6,
-    widget=forms.PasswordInput(
-        attrs={'placeholder': 'Confirmer le mot de passe',
-               'class': 'form-control'}))
-
-
-E_MAIL = forms.EmailField(
-    label='Adresse e-mail',
-    widget=forms.EmailInput(
-        attrs={'placeholder': 'Adresse e-mail', 'class': 'form-control'}),
-    max_length=100,
-    error_messages={'invalid': ("L'adresse e-mail est invalide.")},
-    validators=[validators.validate_email])
+# Fields:
 
 
 FIRST_NAME = forms.CharField(
     label='Prénom',
-    widget=forms.TextInput(
-        attrs={'placeholder': 'Prénom', 'class': 'form-control'}),
-    max_length=30,
+    max_length=150,
     min_length=3,
-    validators=[validators.validate_slug])
+    validators=[validators.validate_slug],
+    widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Prénom'}))
 
 
 LAST_NAME = forms.CharField(
     label='Nom',
-    widget=forms.TextInput(
-        attrs={'placeholder': 'Nom', 'class': 'form-control'}),
-    max_length=30,
+    max_length=150,
     min_length=3,
-    validators=[validators.validate_slug])
+    validators=[validators.validate_slug],
+    widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Nom'}))
+
+
+E_MAIL = forms.EmailField(
+    error_messages={'invalid': ("L'adresse e-mail est invalide.")},
+    label='Adresse e-mail',
+    max_length=150,
+    validators=[validators.validate_email],
+    widget=forms.EmailInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Adresse e-mail'}))
+
+
+PASSWORD1 = forms.CharField(
+    label='Mot de passe',
+    max_length=150,
+    min_length=6,
+    widget=forms.PasswordInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Mot de passe'}))
+
+
+PASSWORD2 = forms.CharField(
+    label='Confirmer le mot de passe',
+    max_length=150,
+    min_length=6,
+    widget=forms.PasswordInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Confirmer le mot de passe'}))
+
+
+ORGANISATION = forms.CharField(
+    label='Organisme',
+    max_length=150,
+    min_length=3,
+    widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Organisme'}))
+
+
+ROLE = forms.CharField(
+    label='Organisme',
+    max_length=150,
+    min_length=3,
+    widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Organisme'}))
+
+
+PHONE = forms.CharField(
+    label='Organisme',
+    max_length=150,
+    min_length=3,
+    widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'placeholder': 'Organisme'}))
 
 
 SITE = forms.IntegerField(
@@ -70,45 +94,49 @@ SITE = forms.IntegerField(
         choices=Organisation.objects.all().values_list('id', 'name')))
 
 
-class UserForm(forms.ModelForm):
+# Forms:
 
-    username = USERNAME
-    password1 = PASSWORD1
-    password2 = PASSWORD2
+
+class UserForm(forms.Form):
+
     email = E_MAIL
     first_name = FIRST_NAME
     last_name = LAST_NAME
+    password1 = PASSWORD1
+    password2 = PASSWORD2
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2',
-                  'first_name', 'last_name', 'email']
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
 
 
 class UserProfileForm(forms.ModelForm):
+
+    organisation = ORGANISATION
+    phone = PHONE
+    role = ROLE
+
     class Meta:
         model = Profile
-        fields = ['orga', 'role', 'phone']
+        fields = ('organisation', 'role', 'phone')
 
 
 class UserDeleteForm(forms.ModelForm):
 
-    username = USERNAME
-
+    email = E_MAIL
     first_name = FIRST_NAME
     last_name = LAST_NAME
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['first_name', 'last_name', 'email']
 
 
 class RegistrationForm(forms.Form):
 
-    username = USERNAME
+    email = E_MAIL
     password1 = PASSWORD1
     password2 = PASSWORD2
-    email = E_MAIL
     site = SITE
 
     def clean(self):
