e8c3834eecd90bc35ae12da5858996730e8bf15c
chakibBH <c.benhabib@neogeo-technologies.fr>
2017-05-04 14:28:52 +0200

    dev profiles

--- profiles/forms.py
+++ profiles/forms.py
@@ -12,6 +12,7 @@ import datetime
 
 
 class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username', 'password', 'email', 'first_name', 'last_name']
