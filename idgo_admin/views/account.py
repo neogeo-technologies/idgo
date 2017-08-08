9f8c6b8404a003f0b9f1f608f189211b28012d29
m431m <m431m@tutanota.com>
2017-08-08 15:59:11 +0200

    Dev

--- idgo_admin/views/account.py
+++ idgo_admin/views/account.py
@@ -30,9 +30,12 @@ from idgo_admin.models import Liaisons_Referents
 from idgo_admin.models import Mail
 from idgo_admin.models import Organisation
 from idgo_admin.models import Profile
-from mama_cas.cas import logout_user
-from mama_cas.models import ServiceTicket
+from mama_cas.compat import is_authenticated as mama_is_authenticated
+from mama_cas.models import ProxyGrantingTicket as MamaProxyGrantingTicket
+from mama_cas.models import ProxyTicket as MamaProxyTicket
+from mama_cas.models import ServiceTicket as MamaServiceTicket
 from mama_cas.utils import redirect as mama_redirect
+from mama_cas.utils import to_bool as mama_to_bool
 from mama_cas.views import LoginView as MamaLoginView
 from mama_cas.views import LogoutView as MamaLogoutView
 
@@ -43,6 +46,26 @@ class SignIn(MamaLoginView):
     template_name = 'idgo_admin/signin.html'
     form_class = SignInForm
 
+    def get(self, request, *args, **kwargs):
+        service = request.GET.get('service')
+        gateway = mama_to_bool(request.GET.get('gateway'))
+        if gateway and service:
+            if mama_is_authenticated(request.user):
+                st = MamaServiceTicket.objects.create_ticket(
+                    service=service, user=request.user)
+                if self.warn_user():
+                    return mama_redirect('cas_warn', params={'service': service, 'ticket': st.ticket})
+                return mama_redirect(service, params={'ticket': st.ticket})
+            else:
+                return mama_redirect(service)
+        elif mama_is_authenticated(request.user):
+            if service:
+                st = MamaServiceTicket.objects.create_ticket(service=service, user=request.user)
+                if self.warn_user():
+                    return mama_redirect('cas_warn', params={'service': service, 'ticket': st.ticket})
+                return mama_redirect(service, params={'ticket': st.ticket})
+        return super().get(request, *args, **kwargs)
+
     def form_valid(self, form):
         login(self.request, form.user)
 
@@ -51,7 +74,7 @@ class SignIn(MamaLoginView):
 
         service = self.request.GET.get('service')
         if service:
-            st = ServiceTicket.objects.create_ticket(
+            st = MamaServiceTicket.objects.create_ticket(
                 service=service, user=self.request.user, primary=True)
             return mama_redirect(service, params={'ticket': st.ticket})
 
@@ -59,7 +82,16 @@ class SignIn(MamaLoginView):
         if nxt_pth:
             return HttpResponseRedirect(nxt_pth)
 
-        return redirect('idgo_admin:home')
+        return mama_redirect('idgo_admin:home')
+
+
+def logout_user(request):
+    if mama_is_authenticated(request.user):
+        MamaServiceTicket.objects.consume_tickets(request.user)
+        MamaProxyTicket.objects.consume_tickets(request.user)
+        MamaProxyGrantingTicket.objects.consume_tickets(request.user)
+        MamaServiceTicket.objects.request_sign_out(request.user)
+        logout(request)
 
 
 class SignOut(MamaLogoutView):
