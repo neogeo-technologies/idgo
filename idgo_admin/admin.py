a826773f2842c2484932c624d2f6bd43ba4e8e18
chakibBH <cbenhabib@neogeo.fr>
2017-12-01 14:59:37 +0100

    dev

--- idgo_admin/admin.py
+++ idgo_admin/admin.py
@@ -39,13 +39,19 @@ class ResourceInline(admin.StackedInline):
     model = Resource
     max_num = 5
     can_delete = True
-    readonly_fields = ('bbox',)
 
     def has_delete_permission(self, request, obj=None):
         return False
 
     def has_add_permission(self, request, obj=None):
         return False
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = list(set(
+            [field.name for field in self.opts.local_fields] +
+            [field.name for field in self.opts.local_many_to_many]
+            ))
+        return readonly_fields
 
 
 class DatasetAdmin(admin.ModelAdmin):
@@ -71,6 +77,19 @@ class DatasetAdmin(admin.ModelAdmin):
         if 'delete_selected' in actions:
             del actions['delete_selected']
         return actions
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['show_save_and_continue'] = False
+        extra_context['show_save'] = False
+        return super(DatasetAdmin, self).changeform_view(request, object_id, extra_context=extra_context)
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = list(set(
+            [field.name for field in self.opts.local_fields] +
+            [field.name for field in self.opts.local_many_to_many]
+            ))
+        return readonly_fields
 
 
 admin.site.register(Dataset, DatasetAdmin)
