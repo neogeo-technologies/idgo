cc6c28424bdcb6f94e145975d92810f6d101be5c
chakibBH <cbenhabib@neogeo.fr>
2018-02-05 14:25:50 +0100

    dev

--- idgo_admin/admin/mail.py
+++ idgo_admin/admin/mail.py
@@ -5,30 +5,31 @@ from idgo_admin.models import Mail
 class MailAdmin(admin.ModelAdmin):
     model = Mail
     ordering = ('subject',)
+    
     # Vue dev:
-    list_display = ('template_name', 'subject', )
-    fieldsets = (
-        ('Personnalisation des messages automatiques',
-         {'fields': ('template_name', 'subject', 'message', 'from_email')}),)
+    # list_display = ('template_name', 'subject', )
+    # fieldsets = (
+    #     ('Personnalisation des messages automatiques',
+    #      {'fields': ('template_name', 'subject', 'message', 'from_email')}),)
     # Fin vue dev
 
     # Vue client:
-    # list_display = ('subject', )
-    # fieldsets = (
-    #     ('Personnalisation des messages automatiques',
-    #      {'fields': ('subject', 'message', )}),)
-    #
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-    #
-    # def has_add_permission(self, request, obj=None):
-    #     return False
-    #
-    # def get_actions(self, request):
-    #     actions = super(MailAdmin, self).get_actions(request)
-    #     if 'delete_selected' in actions:
-    #         del actions['delete_selected']
-    #     return actions
+    list_display = ('subject', )
+    fieldsets = (
+        ('Personnalisation des messages automatiques',
+         {'fields': ('subject', 'message', )}),)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super(MailAdmin, self).get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
     # Fin Vue client
 
 
