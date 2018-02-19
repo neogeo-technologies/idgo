b5af097193520e914acf7c9c07520afc4413e0e4
m431m <m431m@tutanota.com>
2018-02-19 16:11:06 +0100

    Ajouté licence

--- idgo_admin/admin/dataset.py
+++ idgo_admin/admin/dataset.py
@@ -1,7 +1,22 @@
+# Copyright (c) 2017-2018 Datasud.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
 from django.contrib import admin
 from django.forms.models import BaseInlineFormSet
 from django.forms import ValidationError
-
 from idgo_admin.models import Dataset
 from idgo_admin.models import Resource
 from idgo_admin.models import ResourceFormats
@@ -13,7 +28,7 @@ class ResourceFormatsAdmin(admin.ModelAdmin):
     def __init__(self, *args, **kwargs):
         super(ResourceFormatsAdmin, self).__init__(*args, **kwargs)
 
-    class Meta:
+    class Meta(object):
         model = Resource
 
 
@@ -74,7 +89,6 @@ class DatasetAdmin(admin.ModelAdmin):
         last_name = obj.editor.last_name
         return "{} {}".format(first_name, last_name.upper())
     name_editor.short_description = "Producteur (propriétaire)"
-
 
     # def save(self, *args, **kwargs):
     #     import ipdb; ipdb.set_trace()
