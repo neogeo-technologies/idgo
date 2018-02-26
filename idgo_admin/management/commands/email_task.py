215da807c272ce67d0e553898ef9402201267857
m431m <m431m@tutanota.com>
2018-02-26 17:16:09 +0100

    Dev

---
+++ idgo_admin/management/commands/email_task.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2017-2018 Datasud.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import csv
+from datetime import datetime
+from django.core.mail import EmailMessage
+from django.core.management.base import BaseCommand
+from idgo_admin.models import Mail
+from idgo_admin.models import Resource
+from idgo_admin.models import Task
+from io import StringIO
+import time
+
+
+ISO_CALENDAR = datetime.now().isocalendar()
+
+
+class Command(BaseCommand):
+
+    help = 'Envoyer un e-mail contenant les dernières Task.'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def handle(self, *args, **options):
+
+        monday = '{} {} 1'.format(ISO_CALENDAR[0], ISO_CALENDAR[1])
+        query = Task.objects.filter(
+            starting__gte=datetime.fromtimestamp(
+                time.mktime(time.strptime(monday, '%Y %W %w'))))
+
+        data = [
+            ('state', 'starting', 'end', 'dataset', 'resource', 'error')]
+        for item in query:
+            resource = Resource.objects.get(id=item.extras['resource'])
+            dataset = resource.dataset
+            data.append((
+                item.state, item.starting.isoformat(),
+                item.end.isoformat(), dataset.name, resource.name,
+                item.extras.get('errors', None)))
+
+        f = StringIO()
+        csv.writer(f).writerows(data)
+
+        mail_instance = Mail.objects.get(template_name='email_task')
+
+        mail = EmailMessage(mail_instance.subject, mail_instance.message, mail_instance.from_email, Mail.admin_mails([]))
+        mail.attach('log.csv', f.getvalue(), 'text/csv')
+        mail.send()
---
+++ idgo_admin/management/commands/sync_resources.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2017-2018 Datasud.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from idgo_admin.models import Resource
+from idgo_admin.models import Task
+
+
+NOW = timezone.now()
+
+
+class Command(BaseCommand):
+
+    help = 'Synchroniser les ressources'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def handle(self, *args, **options):
+        for instance in Resource.objects.all():
+            if instance.dl_url and instance.synchronisation:
+                if self.is_to_synchronized(instance):
+
+                    extras = {
+                        'dataset': instance.dataset.id,
+                        'resource': instance.id}
+
+                    task = Task.objects.create(action=__name__)
+                    try:
+                        instance.save(sync_ckan=True)
+                    except Exception as e:
+                        task.extras = {**extras, **{'error': e.__str__()}}
+                        task.state = 'failed'
+                    finally:
+                        task.end = timezone.now()
+                        task.extras = extras
+                        task.state = 'succesful'
+                        task.save()
+
+    def is_to_synchronized(self, instance):
+        return {
+            'never': None,
+            'daily': True,
+            'weekly': NOW.isoweekday() == 1,
+            'bimonthly': NOW.day in (1, 15),
+            'monthly': NOW.day == 1,
+            'quarterly': NOW.day == 1 and NOW.month in (1, 4, 7, 10),
+            'biannual': NOW.day == 1 and NOW.month in (1, 7),
+            'annual': NOW.day == 1 and NOW.month == 1
+            }.get(instance.sync_frequency, None)
