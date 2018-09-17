90cecafba719c1722ca2a09075092aafe6aa4f59
m431m <m431m@tutanota.com>
2018-09-17 09:19:05 +0200

    Ajouté licence en-tête de fichier + pep8

--- idgo_admin/auth_ogc.py
+++ idgo_admin/auth_ogc.py
@@ -1,16 +1,32 @@
+# Copyright (c) 2017-2018 Datasud.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import django
+from django.contrib.auth.models import User
+from idgo_admin.models import Resource
 import logging
 import os
 
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
-from django.contrib.auth.models import User
-from idgo_admin.models import Resource
-
-AUTHORIZED_PREFIX = ['/maps/', '/wfs/', '/wms/', '/wxs/']
-
 logger = logging.getLogger('auth_ogc')
 logger.setLevel(logging.DEBUG)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+AUTHORIZED_PREFIX = ['/maps/', '/wfs/', '/wms/', '/wxs/']
 
 
 def check_password(environ, user, password):
