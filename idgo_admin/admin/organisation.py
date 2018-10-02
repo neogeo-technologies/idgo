89a9485ae6c259265b1285457f16096358bd448a
m431m <m431m@tutanota.com>
2018-10-02 16:08:16 +0200

    Corrigé erreur sur 'admin'

--- idgo_admin/admin/organisation.py
+++ idgo_admin/admin/organisation.py
@@ -47,7 +47,7 @@ class OrganisationAdmin(geo_admin.OSMGeoAdmin):
     form = OrganisationForm
 
     def get_form(self, request, obj=None, **kwargs):
-        if not request.user.profile.is_crige_admin:
+        if not request.user.is_superuser and not request.user.profile.is_crige_admin:
             self.form._meta.exclude = ('is_crige_partner',)
         return super().get_form(request, obj, **kwargs)
 
