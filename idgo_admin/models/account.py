7827044de611cf3ee1b2476c9cfe44bf5f8c8ad4
m431m <m431m@tutanota.com>
2018-11-09 15:26:22 +0100

    Correction

--- idgo_admin/models/account.py
+++ idgo_admin/models/account.py
@@ -104,9 +104,9 @@ class Profile(models.Model):
                 profile=self,
                 validated_on__isnull=False).exists()
 
-        return {"is_admin": self.is_admin,
-                "is_referent": is_referent,
-                "is_editor": (self.user == dataset.editor) if dataset else False}
+        return {'is_admin': self.is_admin,
+                'is_referent': is_referent,
+                'is_editor': (self.user == dataset.editor) if dataset else False}
 
     @classmethod
     def get_crige_membership(cls):
