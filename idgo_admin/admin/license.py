1ad65113e59994a330a70cbaff095b3c358ed97a
chakib <cbenhabib@neogeo.fr>
2019-03-28 15:53:50 +0100

    init admin licence delete

--- idgo_admin/admin/license.py
+++ idgo_admin/admin/license.py
@@ -16,10 +16,33 @@
 
 from django.contrib import admin
 from idgo_admin.models import License
+from django.contrib.admin.utils import get_deleted_objects
+from django.db import router
 
 
 class LicenseAdmin(admin.ModelAdmin):
     ordering = ['title']
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        if obj:
+            # Ici on récupere brutalement les objets suscebtibles d'etre supprimés
+            # si on aurai supprimé l'instance courante
+            opts = self.model._meta
+            using = router.db_for_write(self.model)
+            (deleted_objects, model_count, perms_needed, protected) = get_deleted_objects(
+                [obj], opts, request.user, self.admin_site, using)
+            # Si on ne retrouve uniquement que l'instance courante parmis
+            # les objets a supprimés alors on autorise.
+            if len(deleted_objects) == 1:
+                return True
+
+        return False
 
 
 admin.site.register(License, LicenseAdmin)
