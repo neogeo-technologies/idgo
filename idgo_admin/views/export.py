8f76785c7ee514c1c5e96cc61f6460d4f7d75b87
m431m <m431m@tutanota.com>
2019-04-23 11:44:32 +0200

    6748

--- idgo_admin/views/export.py
+++ idgo_admin/views/export.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.contrib.postgres.aggregates import StringAgg
 from django.db.models import Case
 from django.db.models import CharField
+from django.db.models import Count
 from django.db.models import F
 from django.db.models import Func
 from django.db.models.functions import Concat
@@ -36,9 +37,11 @@ from idgo_admin.ckan_module import CkanHandler
 from idgo_admin.exceptions import ExceptionsHandler
 from idgo_admin.exceptions import ProfileHttp404
 from idgo_admin.models import Dataset
+from idgo_admin.models import LiaisonsReferents
 from idgo_admin.models import Profile
 from idgo_admin.shortcuts import on_profile_http404
 from idgo_admin.views.dataset import get_filtered_datasets
+from operator import ior
 import unicodecsv
 from urllib.parse import urljoin
 from uuid import UUID
@@ -60,16 +63,23 @@ DIFFUSEUR = Case(
         then=F('support__name')),
     default=Value(settings.DEFAULT_PLATFORM_NAME),
     output_field=CharField())
-PRODUCTEUR = F('organisation__legal_name')
-COUV_SPAT = Value('', output_field=CharField())
+PRODUCTEUR_NOM = F('organisation__legal_name')
+PRODUCTEUR_SIRET = COLL_SIRET
+COUV_SPAT_MAILLE = F('granularity')
+COUV_SPAT_NOM = F('geocover')
 COUV_TEMP_DEBUT = Value('', output_field=CharField())
 COUV_TEMP_FIN = Value('', output_field=CharField())
 DATE_PUBL = F('date_publication')
-FREQ_MAJ = F('update_frequency')
+FREQ_MAJ = Case(*[
+    When(Q(update_frequency=kvp[0]), then=Value(kvp[1]))
+    for kvp in Dataset._meta.get_field('update_frequency').flatchoices],
+    default=Value(''),
+    output_field=CharField())
 DATE_MAJ = F('date_modification')
-MOT_CLES = StringAgg('keywords__name', distinct=True, delimiter=';')
+MOTS_CLES = StringAgg('keywords__name', distinct=True, delimiter=';')
 LICENCE = F('license__title')
-FORMATS = Func(
+NOMBRE_RESSOURCES = Count('resource')
+FORMAT_RESSOURCES = Func(
     StringAgg('resource__format_type__extension', distinct=True, delimiter=';'),
     function='LOWER')
 PROJECTION = F('resource__crs__auth_code')
@@ -82,9 +92,9 @@ URL = Concat(
 DATASUD_ID = F('slug')
 DATASUD_ORGA_ID = F('organisation__slug')
 DATASUD_ORGA_URL = F('organisation__website')
-DATASUD_PRODUCTEUR_NAME = F('owner_name')
+DATASUD_PRODUCTEUR_NOM = F('owner_name')
 DATASUD_PRODUCTEUR_EMAIL = F('owner_email')
-DATASUD_DIFFUSEUR_NAME = F('broadcaster_name')
+DATASUD_DIFFUSEUR_NOM = F('broadcaster_name')
 DATASUD_DIFFUSEUR_EMAIL = F('broadcaster_email')
 DATASUD_COUV_TERR = F('granularity')
 # DATASUD_INSPIRE =
@@ -96,7 +106,7 @@ DATASUD_DATE_CREATION = F('date_creation')
 # ..champs import/export (catalogue source, dernier moissonage, etc.)
 # DATASUD_RESSOURCE_URLS =
 # DATASUD_RESSOURCE_TAILLE =
-DATASUD_RESSOURCE_TYPES = FORMATS  # ???
+DATASUD_RESSOURCE_TYPES = FORMAT_RESSOURCES  # ???
 
 DATASUD_DATASET_VUES = Value('', output_field=CharField())
 DATASUD_RESSOURCES_TELECHARGEMENT = Value('', output_field=CharField())
@@ -119,23 +129,9 @@ class Export(View):
             except Exception:
                 raise ProfileHttp404
 
-        params = request.POST or request.GET
+        qs = request.POST or request.GET
 
-        if not profile:
-            ids = params.get('ids', '').split(',')
-            qs = Dataset.objects.filter(ckan_id__in=[UUID(id) for id in ids])
-        else:
-            mode = params.get('mode')
-            if mode == 'harvested':
-                QuerySet = Dataset.harvested_csw
-            elif mode == 'mine':
-                QuerySet = Dataset.default.filter(editor=user)
-            elif mode == all:
-                QuerySet = Dataset.default
-
-            qs = get_filtered_datasets(QuerySet, params, profile=profile)
-
-        outputformat = params.get('format')
+        outputformat = qs.get('format')
         if not outputformat or outputformat not in ('odl', 'datasud'):
             raise Http404()
 
@@ -147,19 +143,22 @@ class Export(View):
                 ('TITRE', TITRE),
                 ('DESCRIPTION', DESCRIPTION),
                 ('THEME', THEME),
-                ('DIFFUSEUR', DIFFUSEUR),
-                ('PRODUCTEUR', PRODUCTEUR),
-                ('COUV_SPAT', COUV_SPAT),
+                # ('DIFFUSEUR', DIFFUSEUR),
+                ('PRODUCTEUR_NOM', PRODUCTEUR_NOM),
+                ('PRODUCTEUR_SIRET', PRODUCTEUR_SIRET),
+                ('COUV_SPAT_MAILLE', COUV_SPAT_MAILLE),
+                ('COUV_SPAT_NOM', COUV_SPAT_NOM),
                 ('COUV_TEMP_DEBUT', COUV_TEMP_DEBUT),
                 ('COUV_TEMP_FIN', COUV_TEMP_DEBUT),
                 ('DATE_PUBL', DATE_PUBL),
                 ('FREQ_MAJ', FREQ_MAJ),
                 ('DATE_MAJ', DATE_MAJ),
-                ('MOT_CLES', MOT_CLES),
+                ('MOTS_CLES', MOTS_CLES),
                 ('LICENCE', LICENCE),
-                ('FORMATS', FORMATS),
-                ('PROJECTION', PROJECTION),
-                ('LANG', LANG),
+                ('NOMBRE_RESSOURCES', NOMBRE_RESSOURCES),
+                ('FORMAT_RESSOURCES', FORMAT_RESSOURCES),
+                # ('PROJECTION', PROJECTION),
+                # ('LANG', LANG),
                 ('URL', URL)
                 ))
         else:
@@ -170,29 +169,29 @@ class Export(View):
                 ('TITRE', TITRE),
                 ('DESCRIPTION', DESCRIPTION),
                 ('THEME', THEME),
-                ('DIFFUSEUR', DIFFUSEUR),
-                ('PRODUCTEUR', PRODUCTEUR),
-                ('COUV_SPAT', COUV_SPAT),
+                ('PRODUCTEUR_NOM', PRODUCTEUR_NOM),
+                ('PRODUCTEUR_SIRET', PRODUCTEUR_SIRET),
+                ('COUV_SPAT_MAILLE', COUV_SPAT_MAILLE),
+                ('COUV_SPAT_NOM', COUV_SPAT_NOM),
                 ('COUV_TEMP_DEBUT', COUV_TEMP_DEBUT),
                 ('COUV_TEMP_FIN', COUV_TEMP_DEBUT),
                 ('DATE_PUBL', DATE_PUBL),
                 ('FREQ_MAJ', FREQ_MAJ),
                 ('DATE_MAJ', DATE_MAJ),
-                ('MOT_CLES', MOT_CLES),
+                ('MOTS_CLES', MOTS_CLES),
                 ('LICENCE', LICENCE),
-                ('FORMATS', FORMATS),
-                ('PROJECTION', PROJECTION),
-                ('LANG', LANG),
+                ('NOMBRE_RESSOURCES', NOMBRE_RESSOURCES),
+                ('FORMAT_RESSOURCES', FORMAT_RESSOURCES),
                 ('URL', URL),
                 ('DATASUD_ID', DATASUD_ID),
                 # ('DATASUD_MOT_CLES', DATASUD_MOT_CLES),
                 # ('DATASUD_ORGA', DATASUD_ORGA),
                 ('DATASUD_ORGA_ID', DATASUD_ORGA_ID),
                 ('DATASUD_ORGA_URL', DATASUD_ORGA_URL),
-                ('DATASUD_PRODUCTEUR_NAME', DATASUD_PRODUCTEUR_NAME),
-                ('DATASUD_PRODUCTEUR_EMAIL', DATASUD_PRODUCTEUR_EMAIL),
-                ('DATASUD_DIFFUSEUR_NAME', DATASUD_DIFFUSEUR_NAME),
-                ('DATASUD_DIFFUSEUR_EMAIL', DATASUD_DIFFUSEUR_EMAIL),
+                ('DATASUD_PRODUCTEUR_NOM', DATASUD_PRODUCTEUR_NOM),
+                # ('DATASUD_PRODUCTEUR_EMAIL', DATASUD_PRODUCTEUR_EMAIL),
+                ('DATASUD_DIFFUSEUR_NOM', DATASUD_DIFFUSEUR_NOM),
+                # ('DATASUD_DIFFUSEUR_EMAIL', DATASUD_DIFFUSEUR_EMAIL),
                 ('DATASUD_COUV_TERR', DATASUD_COUV_TERR),
                 # ('DATASUD_INSPIRE', DATASUD_INSPIRE),
                 # ('DATASUD_DATASET_URL', DATASUD_DATASET_URL),
@@ -204,18 +203,47 @@ class Export(View):
                 ('DATASUD_DATASET_VUES', DATASUD_DATASET_VUES),
                 ('DATASUD_RESSOURCES_TELECHARGEMENT', DATASUD_RESSOURCES_TELECHARGEMENT),
                 ('DATASUD_DATASET_NOTE', DATASUD_DATASET_NOTE),
-                ('DATASUD_DATASET_NB_NOTES', DATASUD_DATASET_NB_NOTES)
+                ('DATASUD_DATASET_NB_NOTES', DATASUD_DATASET_NB_NOTES),
+                ('DIFFUSEUR', DIFFUSEUR),
+                ('PROJECTION', PROJECTION),
+                ('LANG', LANG),
                 ))
 
         values = list(annotate.keys())
+
+        if not profile:
+            ids = qs.get('ids', '').split(',')
+            datasets = Dataset.objects.filter(ckan_id__in=[UUID(id) for id in ids])
+        elif 'mode' in qs:
+            mode = qs.get('mode')
+            if mode == 'all':
+                roles = profile.get_roles()
+                if roles['is_admin']:
+                    QuerySet = Dataset.default.all()
+                elif roles['is_referent']:
+                    kwargs = {'profile': profile, 'validated_on__isnull': False}
+                    organisation__in = set(
+                        instance.organisation for instance
+                        in LiaisonsReferents.objects.filter(**kwargs))
+                    filter = ior(Q(editor=user), Q(organisation__in=organisation__in))
+                    QuerySet = Dataset.default.filter(filter)
+            elif mode == 'mine':
+                QuerySet = Dataset.default.filter(editor=user)
+            elif mode == 'ckan_harvested':
+                QuerySet = Dataset.harvested_ckan
+            elif mode == 'csw_harvested':
+                QuerySet = Dataset.harvested_csw
+            else:
+                raise Http404()
+            datasets = get_filtered_datasets(QuerySet, qs)
 
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename=dataset_export.csv'
         response['Cache-Control'] = 'no-cache'
 
-        writer = unicodecsv.writer(response, encoding='utf-8', quoting=csv.QUOTE_ALL, delimiter=';', quotechar='"')
+        writer = unicodecsv.writer(response, encoding='utf-8', quoting=csv.QUOTE_ALL, delimiter=',', quotechar='"')
         writer.writerow(values)
-        for row in qs.annotate(**annotate).values(*values):
+        for row in datasets.annotate(**annotate).values(*values):
             if not outputformat == 'odl':
                 package = CkanHandler.get_package(str(row['ID']), include_tracking=True)
 
