0e22873dd6927e2c96623d15f34c7bf99b087e9a
Sébastien DA ROCHA <sebastien@da-rocha.net>
2019-08-29 08:59:38 +0200

    Fix setup when locale not utf-8

--- setup.py
+++ setup.py
@@ -7,10 +7,16 @@ def get_requirements():
     return [req for req in reqs if not req.startswith('-e')]
 
 
+def get_long_description():
+    with open('README.md', 'rb') as desc_file:
+        description = desc_file.read().decode('utf-8')
+    return description
+
+
 setup(
     descrtiption="Site d'administration d'IDGO",
     packages=find_packages(),
-    long_description=open('README.md').read(),
+    long_description=get_long_description(),
     url='https://github.com/neogeo-technologies/idgo',
     install_requires=get_requirements()
 )
