fa7337571d610d17ac1e8694dafad50561ef9bff
Sébastien DA ROCHA <sebastien@da-rocha.net>
2019-08-29 09:09:31 +0200

    setup ajout du nom du paquet

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ def get_long_description():
 
 
 setup(
+    name="idgo",
     descrtiption="Site d'administration d'IDGO",
     packages=find_packages(),
     long_description=get_long_description(),
