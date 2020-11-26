5891d85f77baba89fac4fa537032c0d32ccdbd1f
m431m <mmeliani@neogeo.fr>
2020-11-26 11:53:42 +0100

    Fixed commands 'fix_resources_bbox_with_layers.py'

--- idgo_admin/management/commands/fix_resources_bbox_with_layers.py
+++ idgo_admin/management/commands/fix_resources_bbox_with_layers.py
@@ -33,9 +33,7 @@ def get_bbox(table_name):
     sql = """
 SELECT ST_AsText(ST_Transform(ST_Envelope(ST_Union(ST_Envelope(the_geom))), 4171))
 AS table_extent FROM {table_name};
-""".format(
-        table_name=table_name,
-        )
+""".format(table_name=table_name)
 
     with connections[IDGO_GEOGRAPHIC_LAYER_DB_NAME].cursor() as cursor:
         try:
@@ -57,19 +55,53 @@ class Command(BaseCommand):
         super().__init__(*args, **kwargs)
 
     def handle(self, *args, **options):
-        for resource in Resource.objects.all():
+        total = Resource.objects.all().count()
+        count = 0
+        for resource in Resource.objects.all().order_by('id'):
+            count += 1
+            logger.info("[%d/%d] - Checking: %s | %s" % (
+                count, total, resource.pk, resource.ckan_url))
             layers = resource.get_layers()
             if layers:
                 toggle = False
                 for layer in layers:
-                    if not layer.type == 'vector':
+                    try:
+                        if not layer.type == 'vector':
+                            continue
+
+                        x_min, y_min, x_max, y_max = layer.bbox.extent
+                        if (x_min >= -180 or x_min <= 180) and \
+                                (x_max >= -180 or x_max <= 180) and \
+                                (y_min >= -90 or y_min <= 90) and \
+                                (y_max >= -90 or y_max <= 90):
+
+                            logger.info("[%d/%d] - BBox for %s is OK: %s" % (
+                                count, total, layer.pk, str(layer.bbox.extent)))
+                            continue
+
+                        logger.warn("[%d/%d] - BBox for %s is not OK: %s" % (
+                            count, total, layer.pk, str(layer.bbox.extent)))
+                        logger.info("[%d/%d] - Proceed to fix it." % (count, total))
+
+                        bbox = get_bbox(layer.name)
+                        layer.bbox = bbox
+                        logger.info("[%d/%d] - Saving Layer %s" % (
+                            count, total, layer.pk))
+                        layer.save()
+                        toggle = True
+                    except Exception as e:
+                        logger.exception(e)
+                        logger.error("[%d/%d] - ERROR: %s" % (
+                            count, total, e.__str__()))
                         continue
-                    bbox = get_bbox(layer.name)
-                    layer.bbox = bbox
-                    logger.info("Saving Layer %s" % layer.pk)
-                    layer.save()
-                    toggle = True
 
                 if toggle:
-                    logger.info("Saving Resource %s" % resource.pk)
-                    resource.save(synchronize=True, skip_download=True)
+                    logger.info("[%d/%d] - Saving Resource %s" % (
+                        count, total, resource.pk))
+                    try:
+                        resource.save(synchronize=True, skip_download=False)
+                    except Exception as e:
+                        logger.exception(e)
+                        logger.error("[%d/%d] - ERROR: %s" % (
+                            count, total, e.__str__()))
+                        continue
